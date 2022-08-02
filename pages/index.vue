7a9861d95051b5bb090b5186ed88125ce5bec9fb
Zongwei_93 <soyayong@gmail.com>
2022-08-02 19:50:41 +0800

    Update homepage

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
       <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <h3 class="text-5xl">My name is Yong Zong Wei</h3>
+      <h3 class="text-5xl">I'm Yong Zong Wei</h3>
       <p class="mt-5">Start from 2018 and enjoy cycling with my road bike -- 享受骑公路自行车的快感</p>
       <img src="https://filedn.com/lzQz4YwK2qQSa8Al3y8Jwsk/Resume%20Image/cycling.jpg" alt="cycling" width="600" height="500">
     </div>
