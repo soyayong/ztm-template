8e96678ecab84bae1b68ef35427238d811613c5b
Zongwei_93 <soyayong@gmail.com>
2022-08-11 10:06:49 +0800

    Update MenuPages

--- components/MenuItem.vue
+++ components/MenuItem.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="text-center">
         <div>
-            <img src="crispy-chicken.jpg" alt="" class="border border-gray p-5" />
+            <img :src="item.image" alt="" class="border border-gray p-5" />
         </div>
         <h4 class="font-owsald uppercase text-2x1 mt-5">
-            Crispy Chicken
+            {{ item.name }}
         </h4>
         <p class="font-light text-gray-500 my-2">
-            Chicken breast, chilli sauce, tomatoes, pickles, coleslaw
+            {{ item.description }}
         </p>
         <p class="mt-5 mb-8">
             <span
                 class="rounded rounded-lg bg-amber-800 text-yellow-400
                 font-oswald font-bold text-xl px-5 py-2"
             >
-                RM 11.99
+                RM {{ item.price.toFixed(2) }}
             </span>
         </p>
         <button class="buttopn font-oswald uppercase border py-2 px-3">
@@ -22,3 +22,9 @@
         </button>
     </div>
 </template>
+
+<script>
+export default {
+  props: ['item'],
+}
+</script>
--- pages/menu.vue
+++ pages/menu.vue
@@ -21,10 +21,29 @@
             </h1>
         </div>
 
-        <div id="items" class="grid grid-cols-3">
-            <MenuItem />
-            <MenuItem />
-            <MenuItem />
+        <div id="items" class="grid grid-cols-3 gap-x-5 gap-y-16 mt-10 mx-20">
+            <MenuItem
+            v-for="b in items"
+            :key="b.name"
+            :item="b"
+            />
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [],
+    };
+  },
+  mounted() {
+    this.$axios.get('items.json').then(
+      (response) => {
+        this.items = response.data.burgers;
+      }
+    );
+  },
+};
+</script>
